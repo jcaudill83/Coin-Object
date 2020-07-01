7ea03cdff0b93d2843c64e6d0aa7938dd40abcbf
Justin Caudill <jcaudill_2003@yahoo.com>
2020-07-01 08:48:13 -0400

    Second Try

--- main.js
+++ main.js
@@ -1,38 +1,46 @@
 let coin = {
     state: 0,
     flip: function() {
-        for (let index = 0; index <= this.state; index += 1) {}
+        var d = Math.random();
+if (d < 0.5){
+    this.state = 0
+}else {
+    this.state = 1
+}
+        
 
         /* 1. Randomly set your coin object's "state" property to be either 
            0 or 1: use "this.state" to access the "state" property on this object.*/
     },
     toString: function() {
-        coin.flip ()
-        if (this.state === 1)
-        if (this.state <= 1) {
-            this.state = ["heads"]
+        if (this.state === 1) {
+            return "heads"
+        } else {
+            return "tails"
         }
+        
 
         /* 2. Return the string "Heads" or "Tails", depending on whether
            "this.state" is 0 or 1. */
     },
     toHTML: function() {
         let image = document.createElement('img');
-        coin.flip ()
         if (this.state === 1) {
             
-            image.src = 'https://magoosh.com/statistics/files/2018/01/quarter-dollar.jpg'
-            mainInput.append(img)
+            image.src = 'images/face-up.jpg'
+            
         }else {
-            image.src = 'https://coinweek.com/wp-content/uploads/2019/02/pezhead.jpg'
-            mainInput.append(img)
+            image.src = 'images/face-down.jpg'
+            
             
         }
     
         /* 3. Set the properties of this image element to show either face-up
            or face-down, depending on whether this.state is 0 or 1.*/
-        // return image;
+        return image;
     }
-    console.log(img);
     
+    
+       
 }
+
